0bb773a18e625bf7df6f5d749649895d0afada35
dab10 <dab10@list.ru>
2023-01-13 01:55:22 +0300

    feat: add response error 501

--- src/index.ts
+++ src/index.ts
@@ -6,17 +6,24 @@ const server = http.createServer(async (req, res) => {
   try {
     if (req.url === '/api/users' && req.method === 'GET') {
       await getUsers(req, res);
+    } else if (req.url === '/api/users' && req.method === 'POST') {
+      await createUser(req, res); 
     } else if (req.url && req.url.split('/').length === 4 && req.url.split('/')[1] === 'api' && req.url.split('/')[2] === 'users' && req.method === 'GET') {
       const id = req.url.split('/')[3];
       await getUser(req, res, id);
-    } else if (req.url === '/api/users' && req.method === 'POST') {
-      await createUser(req, res); 
     } else if (req.url && req.url.split('/').length === 4 && req.url.split('/')[1] === 'api' && req.url.split('/')[2] === 'users' && req.method === 'PUT') {
       const id = req.url.split('/')[3];
       await updateUser(req, res, id);
     } else if (req.url && req.url.split('/').length === 4 && req.url.split('/')[1] === 'api' && req.url.split('/')[2] === 'users' && req.method === 'DELETE') {
       const id = req.url.split('/')[3];
       await deleteUser(req, res, id);
+    } else if (
+        (req.url === '/api/users' && req.method !== 'GET') || 
+        (req.url === '/api/users' && req.method !== 'POST') ||
+        (req.url && req.url.split('/').length === 4 && req.url.split('/')[1] === 'api' && req.url.split('/')[2] === 'users' && !['GET', 'PUT', 'DELETE'].some((item) => item === req.method))
+      ) {
+      res.writeHead(501, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify({ code: 501, message: `server does not support entered request with type of method ${req.method}`}));
     } else {
       res.writeHead(404, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ code: 404, message: 'Route Not Found'}));
