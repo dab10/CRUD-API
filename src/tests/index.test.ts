cf5c3072b61d87eea649ea28c86ad47402c776cb
dab10 <dab10@list.ru>
2023-01-16 22:36:05 +0300

    feat: add second test

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -29,7 +29,7 @@ afterAll(() => {
 });
 
 describe('API Tests', function () {
-  describe('Scenario N1 - Get all users => Create user => Get create user => update user => Delete updated => get deleted', function () {
+  describe('Scenario N1 - Get all users => Create user => Get create user => Update user => Delete updated => Get deleted', function () {
     let userId: string;
 
     it('should return empty array of users', async () => {
@@ -73,6 +73,61 @@ describe('API Tests', function () {
       expect(res.body).toEqual({
         code: 404,
         message: 'User not found',
+      });
+    });
+  });
+
+  describe('Scenario N2 - Create two new users => Read all (2 users) => Create new users (3 users) => Delete second (2 users) => Read all (2 users) => POST bad JSON data', function () {
+    const userIds: string[] = [];
+
+    it('should create two new users', async () => {
+      for (let i = 0; i < 2; i++) {
+        const createNewUser = createUser;
+        createNewUser.hobbies.push(String(i));
+        const res = await request.post('/api/users').send(createNewUser);
+        expect(res.status).toEqual(201);
+        expect(res.body).toEqual(
+          expect.objectContaining({ id: expect.any(String), ...createNewUser })
+        );
+        userIds.push(res.body.id);
+      }
+    });
+
+    it('should return array of two users', async () => {
+      const res = await request.get('/api/users');
+      expect(res.status).toEqual(200);
+      expect(res.body.users.length).toEqual(2);
+    });
+
+    it('should return created object of user with id', async () => {
+      const res = await request.post('/api/users').send(createUser);
+      expect(res.status).toEqual(201);
+      expect(res.body).toEqual(
+        expect.objectContaining({ id: expect.any(String), ...createUser })
+      );
+      userIds.push(res.body.id);
+    });
+
+    it('should delete second user by id and return 204', async () => {
+      const res = await request.delete('/api/users/' + userIds[1]);
+      userIds.splice(1, 1);
+      expect(res.status).toEqual(204);
+      expect(res.body).toEqual('');
+    });
+
+    it('should return array of two users', async () => {
+      const res = await request.get('/api/users');
+      expect(res.status).toEqual(200);
+      expect(res.body.users.length).toEqual(2);
+      expect(res.body.users[1].hobbies).toEqual(['hackers', '0', '1']);
+    });
+
+    it('should return internal error msg', async () => {
+      const res = await request.post('/api/users').send(badUser);
+      expect(res.status).toEqual(400);
+      expect(res.body).toEqual({
+        code: 400,
+        message: 'Body does not contain required fields (username, age, hobbies) or properties do not match data types',
       });
     });
   });
